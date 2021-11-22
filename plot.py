a023297fbcb22247d6fb2fc79180fcb4c822fe75
yolomcswag <github.r159q@slmail.me>
2021-11-22 16:13:19 +0100

    add basline accuracy to plot

--- plot.py
+++ plot.py
@@ -13,19 +13,21 @@ def main():
                 experiment_list_of_dicts.append(result_dict)
     
     experiment_list_of_dicts.sort(key=lambda d: d['experiment_name'])
-    names = [experiment_dict["experiment_name"][0:7] for experiment_dict in experiment_list_of_dicts]
+    names = [experiment_dict["experiment_name"][0:6] for experiment_dict in experiment_list_of_dicts]
     accuracies = [experiment_dict["test_accuracy"] for experiment_dict in experiment_list_of_dicts]
 
+    #add the claimed accuracy from https://pytorch.org/tutorials/intermediate/spatial_transformer_tutorial.html
+    names = ["baseline"] + names
+    accuracies = [98.69] + accuracies
+
     fig, ax = plt.subplots()
-    # plt.figure(figsize=(40,10))
     plt.title('Accuracy Comparison')
     ax.yaxis.labelpad = 40
     plt.xlabel("Test Accuracy [%]")
     plt.xlim( (math.floor(min(accuracies)), 100))
     plt.barh(names[::-1],accuracies[::-1])
     plt.tight_layout()
-    plt.savefig("test.png")
-    a  = 1
+    plt.savefig("AccuracyComparison.png")
     
 
 if __name__ == "__main__":
